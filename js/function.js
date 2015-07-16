eb5446f1a74dccafaf61474558263150063266b6
Vũ Đình Đông <vudinhdong.it1710@gmail.com>
2015-07-16 10:46:41 +0700

    ver 1.0

---
+++ js/function.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+
+    $('.block-01 .flexslider').flexslider({
+        animation: "slide"
+    });
+
+    $('.block-02 .flexslider').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails"
+    });
+});
